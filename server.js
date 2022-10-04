812ca40368df2d147fc6c5e4547820178be8f650
JBrendon15 <110281109+JBrendon15@users.noreply.github.com>
2022-10-03 23:51:30 -0500

    added switch cases based on user selection on first prompt and add a function for view all roles

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mysql = require('mysql2');
 const app = express();
 const cTable = require('console.table');
+const inquirer = require('inquirer');
 const PORT = process.env.PORT || 3001;
 
 
@@ -11,17 +12,78 @@ app.use(express.urlencoded({ extended: true }));
 const db = mysql.createConnection(
     {
         host: 'localhost',
-        // MySQL username,
         user: 'root',
         password: 'rootroot',
         database: 'employeetracker_db'
     },
-    console.log(`Connected to the employeetracker_db database.`)
 );
 
-db.query('SELECT * FROM role', function (err, results) {
-    console.table(results)
+function init() {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                message: 'What would you like to do?',
+                choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Quit'],
+                name: 'whatDo'
+            }
+        ])
+        .then((answer) => {
+            switch (answer.whatDo) {
+                case 'View All Employees':
+                    viewEmployees();
+                    break;
+                case 'Add Employee':
+                    addEmployee();
+                    break;
+                case 'Update Employee Role':
+                    updateEmployee();
+                    break;
+                case 'View All Roles':
+                    viewRoles();
+                    break;
+                case 'Add Role':
+                    addRole();
+                    break;
+                case 'View All Departments':
+                    viewDepartments();
+                    break;
+                case 'Add Department':
+                    addDepartment();
+                    break;
+                case 'Quit':
+                    break;
+            }
+        })
+}
+
+function viewEmployees() {
+
+}
+
+function addEmployee() {
+    
+}
+
+function updateEmployee() {
+
+}
+
+function viewRoles() {
+    db.query('SELECT role.id, role.title, department.name AS department, role.salary FROM role JOIN department ON role.department_id = department.id', function (err, results) {
+        console.table(results)
+    })
+}
+
+function addRole() {
+
+}
+
+function viewDepartments() {
+
+}
+
+app.listen(PORT, () => {
+    console.log(`Listening on PORT: ${PORT}`); 
+    init();
 })
-
-
-app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
